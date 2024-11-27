0fde7d9b6a5f4c0455daa765360750e80f76d4e6
hajhi7899 <hajhi7899@gmail.com>
2024-11-27 23:59:49 +0900

    드라이브 생성

--- src/main/java/com/backend/controller/DriveController.java
+++ src/main/java/com/backend/controller/DriveController.java
@@ -1,14 +1,30 @@
 package com.backend.controller;
 
 
+import com.backend.dto.request.drive.NewDriveRequest;
+import com.backend.service.FolderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Log4j2
 @CrossOrigin(origins = "http://localhost:8010")
+@RequestMapping("/api/drive")
 @RequiredArgsConstructor
 public class DriveController {
+
+
+    private final FolderService folderService;
+
+    @PostMapping("/newDrive")
+    public void createDrive(@RequestBody NewDriveRequest newDriveRequest) {
+        log.info("New drive request: " + newDriveRequest);
+
+        folderService.createDrive(newDriveRequest);
+
+
+
+    }
 }
