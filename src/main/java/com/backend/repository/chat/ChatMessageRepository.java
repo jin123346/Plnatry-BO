95ef93ec4c9ec3d69637a4b159d78e1ba3dea0d7
gyuchanJeon <ianjeon810900@gmail.com>
2024-11-28 17:49:24 +0900

    채팅 관련 파일들 + 몽고디비 세팅

---
+++ src/main/java/com/backend/repository/chat/ChatMessageRepository.java
@@ -0,0 +1,9 @@
+package com.backend.repository.chat;
+
+import com.backend.document.ChatMessageDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatMessageRepository extends MongoRepository<ChatMessageDocument, Long> {
+}
