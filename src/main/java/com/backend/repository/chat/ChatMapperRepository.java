8b8f430e90f661a3b302669c7007696067f6b7c7
gyuchanJeon <ianjeon810900@gmail.com>
2024-12-09 17:55:50 +0900

    채팅 내역 조회

---
+++ src/main/java/com/backend/repository/chat/ChatMapperRepository.java
@@ -0,0 +1,13 @@
+package com.backend.repository.chat;
+
+import com.backend.document.chat.ChatMapperDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChatMapperRepository extends MongoRepository<ChatMapperDocument, String> {
+    Optional<ChatMapperDocument> findByUserIdAndChatRoomId(String userId, String chatRoomId);
+    void deleteByUserIdAndChatRoomId(String userId, String chatRoomId);
+}
