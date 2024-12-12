c5951d21d3ae56e2d3c37801ea94d4669691be4f
hajhi7899 <hajhi7899@gmail.com>
2024-12-13 00:11:17 +0900

    드라이브

--- src/main/java/com/backend/service/UserService.java
+++ src/main/java/com/backend/service/UserService.java
@@ -402,6 +402,24 @@ public class UserService {
     }
 
 
+    public UserDto getSliceUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("유저 정보를 찾을 수 없습니다."));
+        Group group = user.getGroupMappers().stream()
+                .map(GroupMapper::getGroup)
+                .findFirst()
+                .orElse(null);
+        UserDto userDto= user.toSliceDto();
+        if(group != null){
+            String department = group.getName();
+            long groupId = group.getId();
+            userDto.setDepartment(department);
+            userDto.setGroupId(groupId);
+        }
+        return userDto;
+    }
+
+
+
 //    public Boolean uploadProfile(Long userId, MultipartFile file) {
 //
 //        User user = userRepository.findById(userId);
